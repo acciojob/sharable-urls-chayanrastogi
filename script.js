85d78f1cb4051596d443bf8d6412bf6c716ad3b9
Chayan Rastogi <80814459+chayanrastogi@users.noreply.github.com>
2023-04-25 13:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// your code here
+function updateUrl() {
+			var name = document.getElementById("name").value;
+			var year = document.getElementById("year").value;
+			var url = "https://localhost:8080/?name=" + name + "&year=" + year;
+			document.getElementById("url").textContent = url;
+}
