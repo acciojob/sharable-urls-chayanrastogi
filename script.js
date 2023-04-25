1a63254f088f35ba29bb2f482f8adeb60ac313d7
Chayan Rastogi <80814459+chayanrastogi@users.noreply.github.com>
2023-04-25 14:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function updateUrl() {
 			var name = document.getElementById("name").value;
 			var year = document.getElementById("year").value;
-			var url = "https://localhost:8080/?name=" + name + "&year=" + year;
+		    if(name !=="" && year==="")
+				var url = "https://localhost:8080/?name=" + name;
+			else if(year!=="" && name==="")
+				var url = "https://localhost:8080/?year=" + year;
+			else if(year!=="" && name!=="")
+				var url = "https://localhost:8080/?name=" + name + "&year=" + year;
 			document.getElementById("url").textContent = url;
 }
